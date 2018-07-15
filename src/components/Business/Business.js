afc095b9c57dfe8b2b196c2f5ccfb4fd0162e885
kedarjoyner <kedarjoyner@gmail.com>
2018-07-15 16:16:35 -0500

    Change filepaths and add SearchBar component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -5,7 +5,7 @@
 */
 
 import React from 'react';
-import 'Business.css';
+import './Business.css';
 
 let business = {
     imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',

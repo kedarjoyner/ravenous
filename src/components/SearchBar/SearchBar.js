0d47065de22929d38b4d718ce43d1a9d7b1a8c75
kedarjoyner <kedarjoyner@gmail.com>
2018-07-20 23:04:41 -0500

    Add methods to simulate search functionality

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -16,6 +16,9 @@ class SearchBar extends React.Component{
             location: '',
             sortBy: 'best_match'
         };
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
     }
     // Return active css class for a sorting option 
     getSortByClass(sortByOption) {
@@ -31,6 +34,25 @@ class SearchBar extends React.Component{
         this.setState({
             sortBy: sortByOptions
         });
+    }
+    // State of input element should be updated to reflect text type into the input
+    handleTermChange(event){
+        this.setState({
+            term: event.target.value
+        });
+    }
+    // State of input element should be updated to reflect text type into the input
+    handleLocationChange(event){
+        this.setState({
+            location: event.target.value
+        });
+    }
+
+    // Handle search
+    handleSearch(event){
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+        // Prevent default action of clicking a link from triggering at end of method
+        event.preventDefault();
     }
     renderSortByOptions(){
         return Object.keys(sortByOptions).map(sortByOption => {
@@ -50,11 +72,11 @@ class SearchBar extends React.Component{
                 </ul>
             </div>
             <div className="SearchBar-fields">
-                <input placeholder="Search Businesses" />
-                <input placeholder="Where?" />
+                <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+                <input onChange={this.handleLocationChange} placeholder="Where?" />
             </div>
             <div className="SearchBar-submit">
-                <a>Let's Go</a>
+                <a onClick={this.handleSearch}>Let's Go</a>
             </div>
             </div>
 
